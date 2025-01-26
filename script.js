1f78137407b7396cb1708aea10484b7b349bc5e9
disgidyduru <disgidyduru@gmail.com>
2025-01-26 17:04:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-<script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
@@ -22,12 +21,6 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
-
-
-
-
-
 
 
 
